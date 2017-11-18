0dc14e8f8e5eff845861cb205ecc12ec8efb615e
Eyo Okon Eyo <eyo.okon@konga.com>
2017-11-18 16:34:20 +0100

    scaffold build tasks, and routes configuration

--- server.js
+++ server.js
@@ -6,12 +6,22 @@ const cons = require('consolidate');
 const app = express();
 const PORT = 3000;
 
+
+// assign the dust engine to .dust files
+app.engine('dust', cons.dust);
+app.set('view engine', 'dust');
+
+//serve public assets
+app.use(express.static('./public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+
+
 require('routes/')(app);
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
 
     console.log(`App is listening on ${PORT} `);
 
