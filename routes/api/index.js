b507f23d391af592cba7d076a0346acc017b3d46
Olaide Agboola <olaideagboola@Olaides-MacBook-Pro.local>
2017-11-19 15:26:11 +0100

    WIP: api setup

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,19 +1,28 @@
 'use strict';
+const courses = require('../../app/controllers/courses.controller')
 
 module.exports = () => {
 
     let router = require('express').Router();
 
-    router.get('/', (req, res, next) => {
-        //do Something here
-        res.json({status: 'success', message: 'Hello World'});
-    });
+    router.route('/courses')
+      .get(courses.listCourses)
+      .post(courses.createCourse);
 
     router.get('/getHotels', (req, res, next) => {
         //do Something here
         res.json({status: 'success', message: 'Hello World'});
     });
 
+    router.route('/courses/:courseId')
+      .get(courses.getCourse)
+      .put(courses.updateCourse)
+      .delete(courses.deleteCourse);
+
+    router.route('/tests')
+      .get(courses.getCourse)
+      .put(courses.updateCourse)
+      .delete(courses.deleteCourse);
 
     return router;
 
